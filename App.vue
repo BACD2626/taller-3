d827820692f8fb5f27ea956d5c4838c234f4affd
BACD2626 <brandoncampos2626@gmail.com>
2018-11-30 14:18:37 -0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app" >
+    
+     <div  v-if="mostrar == true  ">
+        <div class="login card-panel green white-text center">
+            <h1>INICIA SECION</h1>
+            <input type=“text” placeholder="usuario" v-model="user" /><br> <br>
+          
+            <input type=“text” placeholder="pass" v-model="pass" /><br> <br>
+            
+
+            <button type="primary" @click="mostrar = false"> login</button>
+        </div>  
+      
+     
+     </div>
+
+      <div v-if="mostrar == false  " >
+     <formulario/>
+     <br><br>
+     <button type="primary" @click="mostrar = true"> salir</button>
+     
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+import formulario from './components/formulario.vue'
+export default {
+  name: 'app',
+  components:{
+     formulario
+  },
+  data () {
+    
+    
+    return {
+      
+      mostrar: true,
+      
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ formulario.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+      <h1>FORMULARIO DE ENTREGA</h1>
+      
+    <input type=“text” placeholder="nombre" v-model="nombre" />
+    
+    <br>
+    <br>
+    
+    <input type=“text” placeholder="apellido" v-model="apellido" />
+   
+    <br>
+    <br>
+     
+    <input type=“text” placeholder="ciudad" v-model="ciudad" />
+    <br>
+    <br>
+    
+    <input type=“text” placeholder="sexo" v-model="sexo" />
+    <br>
+    <br>
+     
+   <input type=“text” placeholder="fecha de nacimiento" v-model="fecha" />
+    <br>
+    <br>
+     
+    <input type=“text” placeholder="direccion de entrega" v-model="direccion" />
+    <br> <br>
+
+    <button type="primary" @click="mostrar = true"> mostrar datos</button>
+    <button type="primary" @click="mostrar = false"> ocultar datos</button>
+    
+    <div  v-if="mostrar == true  ">
+         <h1>datos de cliente</h1>
+         
+         <h2>
+         ---------------------------- <br>   
+         nombre: {{nombre}} <br>
+          ---------------------------- <br>
+         apellido: {{apellido}}<br>
+          ---------------------------- <br>
+         ciudad: {{ciudad}}<br>
+          ---------------------------- <br>
+         sexo: {{sexo}}<br>
+          ---------------------------- <br>
+         fecha: {{fecha}}<br>
+          ---------------------------- <br>
+         direccion: {{direccion}}<br>
+         ---------------------------- 
+         </h2>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name:'',
+  data () {
+    return {
+        mostrar:false,
+        nombre: null,
+        apellido: null,
+        ciudad: null,
+        sexo: null,
+        fecha: null,
+        direccion: null,
+    }
+  },
+  methods:{
+      onclick:function(){
+          console.log(this.nombre)
+
+          console.log(this.apellido)
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
